b67b7d9c96ea10ed9f77965fdd5192911480d52b
Juan Pastas <pastasjuan@gmail.com>
2016-08-27 16:48:45 -0500

    WIP active admin

--- app/admin/client.rb
+++ app/admin/client.rb
@@ -1,2 +1,4 @@
 ActiveAdmin.register Client do
+  permit_params { Client.attribute_names }
+
 end
--- app/admin/feature_request.rb
+++ app/admin/feature_request.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register FeatureRequest do
   permit_params { FeatureRequest.attribute_names }
+
+  controller do
+    def index
+      @feature_requests = FeatureRequest
+        .page(params[:page]).per(params[:per_page])
+    end
+  end
 end
