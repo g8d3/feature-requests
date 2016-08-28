1ca8d93c9265acc58493d17a912ea1c64757e9e0
Juan Pastas <pastasjuan@gmail.com>
2016-08-27 20:11:12 -0500

    Add pry-rails. Order requests.

--- app/admin/feature_request.rb
+++ app/admin/feature_request.rb
@@ -3,8 +3,11 @@ ActiveAdmin.register FeatureRequest do
 
   controller do
     def index
-      @feature_requests = FeatureRequest
-        .page(params[:page]).per(params[:per_page])
+      index! do
+        collection.order('client_priority asc, updated_at desc')
+      end
+      # @feature_requests = FeatureRequest
+      #   .page(params[:page]).per(params[:per_page])
     end
   end
 end
